ff5f7339516f326bfff0099233766d4c5b2af1a9
gostevVitalii <62056973+gostevVitalii@users.noreply.github.com>
2022-03-07 11:00:51 +0300

    Media query

--- script.js
+++ script.js
@@ -3,11 +3,30 @@ const signupBtn = document.querySelector('.signupBtn')
 const formBx = document.querySelector('.formBx')
 const body = document.querySelector('body')
 
-signupBtn.onclick = function () {
+signupBtn.onclick = signUp
+signinBtn.onclick = signIn
+function signUp() {
     formBx.classList.add('active')
     body.classList.add('active')
 }
-signinBtn.onclick = function () {
+function signIn() {
     formBx.classList.remove('active')
     body.classList.remove('active')
 }
+
+let x = window.matchMedia("(min-width: 991px)")
+function arrowToggle(x) {
+    if (x.matches) {
+        document.addEventListener('keyup', (event) => {
+            if (event.key === 'ArrowLeft') {
+                signIn()
+            } else if (event.key === 'ArrowRight') {
+                signUp()
+            }
+        })
+    }
+}
+arrowToggle(x)
+window.addEventListener('resize', () => {
+    arrowToggle()
+})
